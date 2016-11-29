ea58dc2ac1458c2b84d0d2ab2f8274a5f12a35be
Quint436 <quint436@uw.edu>
2016-11-29 14:55:23 -0800

    new

---
+++ 4_basic13/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        //     for(int i = 1; i <= 255; i++)
+        //     {
+        //         Console.WriteLine(i);
+        //     }
+        // }
+        // public static void printOdd255()
+        // {
+        //     for(int i = 1; i <= 255; i += 2)
+        //     {
+        //         Console.WriteLine(i);
+        //     }
+        // }
+        // int sum = 0;
+        // for(int i = 0; i <= 255; i++)
+        // {
+        //     sum += i;
+        //     Console.WriteLine(i + " " + sum);
+        //     Console.WriteLine("New number: {0} Sum: {1}", i, sum);
+        // }
+        // int[] x = {1, 2, 3, 4, 5};
+        // for(int i = 0; i < x.Length; i++){
+        //     Console.WriteLine(x[i]);
+        //     }
+        // }
+        // {
+        // int [] Arr;
+        // Arr = new int[] {2,10,3};
+        // int sum = 0;
+        // for(int i = 0; i < Arr.Length; i++)
+        // {
+        //     sum += Arr[i];
+        // }
+        // int avg = sum/Arr.Length;
+        // Console.WriteLine(avg);
+        // }
+        // {
+        // var oddArray = new int[128];
+
+        // for (int i = 0, odd = 1; i < oddArray.Length; i++, odd += 2)
+        // {
+        //     oddArray[i] = odd;
+        //     Console.WriteLine(oddArray[i]);
+        // }
+        // int [] array;
+        // array = new int[] {1,3,5,7,9,13};
+        // int y = 6;
+        // int count = 0;
+        // for(int i = 0; i < array.Length; i++)
+        // {
+        //     if(array[i] > y)
+        //     {
+        //         count ++;
+        //     }
+        // }
+        // Console.WriteLine(count);
+        // }
+        // {
+        //     int[] array;
+        //     array = new int[] {1,3,5,7,9,-5};
+        //     {
+        //         for(int i = 0; i < array.Length; i++)
+        //         {
+        //             array[i] = array[i]*array[i];
+        //             Console.WriteLine(array[i]);
+        //         }
+        //     }
+        //     }
+        // {
+        //     int [] array;
+        //     array = new int[] {1,5,10,-2};
+        //     for(int i = 0; i < array.Length; i++)
+        //     {
+        //         if(array[i] < 0)
+        //         {
+        //             array[i] = 0;
+        //             Console.WriteLine(array[i]);
+        //         }else{
+        //             Console.WriteLine(array[i]);
+        //         }
+        //         }
+        //     }
+        // {
+        //     int [] array;
+        //     array = new int[] {1,5,10,-2};
+        //     int sum = 0;
+        //     int max = 0;
+        //     int min = 0;
+        //     for(int i = 0; i < array.Length; i++)
+        //     {
+        //         if(max < array[i])
+        //         {
+        //             max = array[i];
+        //         }
+        //         if(min > array[i])
+        //         {
+        //             min = array[i];
+        //         }
+        //         sum = sum += i;
+        //     }
+        //     int avg = sum/array.Length;
+        //     Console.WriteLine("Average: " + avg);
+        //     Console.WriteLine("Min: " + min);
+        //     Console.WriteLine("Max: " + max);
+        // }
+        // {
+        //     int[] array;
+        //     array = new int[] {1,5,10,-2};
+        //     for(int i = 0; i < array.Length; i++)
+        //     {
+        //         array[i] = array[i + 1];
+        //         Console.WriteLine(array[i]);
+        //     }
+        // }
+        {
+            int[] = array;
+            array = new int[] {1,3,5,7,9};
+            for(int i = 0; i < Array.Length; i++) {
+                if(array[i] < 0){
+                    string myString = array[i].ToString();
+                    myString = "Dojo";
+                    Console.WriteLine(myString);
+                }else{
+                    Console.WriteLine(array[i]);
+                }
+                }
+            }
+        }
+    }
+}
