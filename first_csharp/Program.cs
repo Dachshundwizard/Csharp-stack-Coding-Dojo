e1f34274b4286ccf95db6ddc9c210c081cab6fed
Quint436 <quint436@uw.edu>
2016-11-28 16:55:22 -0800

    cool

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -1,4 +1,39 @@
-﻿using System;
+﻿// using System;
+
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             for(int i = 1; i <=255; i++)
+//             {
+//             Console.WriteLine(i);
+//             }
+//         }
+//     }
+// }
+
+// using System;
+
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             for (int i = 1; i <= 100; i++) 
+//             {
+//                 if ((i % 3 == 0 && i % 5 != 0) || (i % 5 == 0 && i % 3 != 0)) 
+//                 {
+//                     Console.WriteLine(i);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+using System;
 
 namespace ConsoleApplication
 {
@@ -6,7 +41,13 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++) 
+            {
+                if ((((i/3.0) - Math.Floor(i/3.0)) == 0.0 && ((i/5.0) - Math.Floor(i/5.0)) != 0.0) || (((i/5.0) - Math.Floor(i/5.0)) == 0.0 && ((i/3.0) - Math.Floor(i/3.0)) != 0.0)) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
